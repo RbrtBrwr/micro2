00470fced3aa906cbc1f17400ba5688fc6fd5d28
Robert Brewer <robertbrew98@gmail.com>
2022-03-12 10:04:26 -0400

    Solo un beso

---
+++ src/App/index.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>Solo</li>
+          <li>Un</li>
+          <li>Beso</li>
+          <li>Para</li>
+          <li>Ti</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
